fb11c266b30ca733d6206831f2154ebfc5dd0ba6
Karurinka <michellebroens96@gmail.com>
2018-04-02 15:29:36 +0200

    added performance tests

---
+++ src/tests/LogicTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+public class LogicTest
+{
+
+    @org.junit.Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @org.junit.Test
+    public void buildFrequencyTable()
+    {
+    }
+
+    @org.junit.Test
+    public void buildHuffmanTree()
+    {
+    }
+
+    @org.junit.Test
+    public void buildLookupTable()
+    {
+    }
+
+    @org.junit.Test
+    public void addToLookupTable()
+    {
+    }
+
+    @org.junit.Test
+    public void generateEncodedData()
+    {
+    }
+
+    @org.junit.Test
+    public void compress()
+    {
+    }
+
+    @org.junit.Test
+    public void decompress()
+    {
+    }
+}
